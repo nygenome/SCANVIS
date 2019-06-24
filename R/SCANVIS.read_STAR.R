b626251e52da11eb46666f8e5a14bb693d83b4e3
Phaedra Agius <phaedragius@gmail.com>
2019-06-24 11:50:33 -0400

    replaced _ in fun name

--- R/SCANVIS.read_STAR.R
+++
@@ -1,8 +0,0 @@
-##INPUT: url to star sj file
-SCANVIS.read_STAR<-function(sj_file){
-    out=as.matrix(read.delim(sj_file,header=FALSE))
-    colnames(out)=c("chr","start","end","strand","intron.motif","is.annotated",
-        "uniq.reads","multi.reads","max.overhang")
-    out=gsub(' ','',out)
-    return(out)
-}
